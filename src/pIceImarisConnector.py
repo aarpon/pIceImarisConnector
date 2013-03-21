0f036b10a2ab73e891a13e8f37dfce3a87f2adcd
Aaron Ponti <aarpon@gmail.com>
2013-03-21 22:32:30 +0100

    Initial import.

---
+++ src/pIceImarisConnector.py
@@ -0,0 +1,40 @@
+'''
+Name:         pIceImarisConnector
+Purpose:      pIceImarisConnector is a simple Python class eases communication
+              between Bitplane Imaris and Python using the Imaris XT interface.
+
+Author:      Aaron Ponti
+
+Created:     21.03.2013
+Copyright:   (c) Aaron Ponti 2013
+Licence:     GPL v2
+'''
+
+class pIceImarisConnector:
+    
+    '''
+    pIceImarisConnector is a simple Python class eases communication between
+    Bitplane Imaris and Python using the Imaris XT interface.
+    '''
+    
+    def __init__(self, imarisApplication = None, indexingStart = 0):
+    
+        ''' 
+        pIceImarisConnector constructor
+
+        Arguments:
+
+        imarisApplication:  <describe> (default None)
+        indexingStart:      <describe> (default 0)
+        '''
+        
+        self._mImarisApplication = imarisApplication
+
+
+    def findImaris(self):
+        
+        ''' 
+        This methods gets the Imaris path to the Imaris executable and
+        to the ImarisLib.jar library from the environment variable IMARISPATH.
+        '''
+        pass
---
+++ src/pIceImarisConnectorTestUnit.py
@@ -0,0 +1,22 @@
+'''
+Name:         pIceImarisApplication Test Unit
+Purpose:      Test pIceImarisApplication
+
+Author:       Aaron Ponti
+
+Created:      21-03-2013
+Copyright:    (c) Aaron Ponti 2013
+Licence:      GPL v2
+'''
+    
+from pIceImarisConnector import pIceImarisConnector
+import unittest
+
+class pIceImarisConnectorUnitTest(unittest.TestCase):
+    
+    def testConstruct(self):
+        assert(True)
+        
+
+if __name__ == '__main__':
+    unittest.main()
